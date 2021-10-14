b2c52bcb0ee23af97c01a54f3cfba5d87b150aa5
gv-cloud <gv-cloud@outlook.com>
2021-10-14 18:13:29 -0400

    Initial Load

---
+++ src/main/java/org/event/driven/development/test/TopicSearch.java
@@ -0,0 +1,15 @@
+package org.event.driven.development.test;
+
+public class TopicSearch {
+
+    /**
+     * This is a sample method.
+     *
+     * @param text a sample parameter
+     * @return returns a sample text
+     */
+    public String searchInfo(final String text) {
+        return "This is a test " + text;
+    }
+
+}
